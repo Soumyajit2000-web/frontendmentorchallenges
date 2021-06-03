7ce8401a10793e4728d3403808dd34d3f1771e0e
Soumyajit2000-web <84496904+Soumyajit2000-web@users.noreply.github.com>
2021-06-03 20:36:47 +0530

    Add files via upload

---
+++ blogr-landing-page-main/script/script.js
@@ -0,0 +1,72 @@
+function toggleDropdown1() {
+
+    let dropdown = document.getElementById('element1');
+    // console.log(dropdown);
+
+    dropdown.classList.toggle('dropdown');
+    dropdown.classList.toggle('dropdown-close');
+}
+
+function toggleDropdown2() {
+
+    let dropdown = document.getElementById('element2');
+    // console.log(dropdown);
+
+    dropdown.classList.toggle('dropdown');
+    dropdown.classList.toggle('dropdown-close');
+
+
+}
+
+function toggleDropdown3() {
+
+    let dropdown = document.getElementById('element3');
+    // console.log(dropdown);
+
+    dropdown.classList.toggle('dropdown');
+    dropdown.classList.toggle('dropdown-close');
+
+
+}
+
+function burgerDropdown1() {
+
+    let dropdown = document.getElementById('burger-drop-element1');
+    // console.log(dropdown);
+
+    dropdown.classList.toggle('dropdown');
+    dropdown.classList.toggle('dropdown-close');
+
+
+}
+
+function burgerDropdown2() {
+
+    let dropdown = document.getElementById('burger-drop-element2');
+    // console.log(dropdown);
+
+    dropdown.classList.toggle('dropdown');
+    dropdown.classList.toggle('dropdown-close');
+
+
+}
+
+function burgerDropdown3() {
+
+    let dropdown = document.getElementById('burger-drop-element3');
+    // console.log(dropdown);
+
+    dropdown.classList.toggle('dropdown');
+    dropdown.classList.toggle('dropdown-close');
+
+
+}
+
+function toggleBurger() {
+    // let burger = getElementById('burger-icon');
+    let container = document.getElementById('burger-cont');
+
+    container.classList.toggle('burger-container-close');
+    container.classList.toggle('burger-container');
+
+}
